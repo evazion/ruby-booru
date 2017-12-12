65db8a1c4cc71072582df4f07ef18a7215cb9b39
evazion <noizave@gmail.com>
2017-12-12 11:05:30 -0600

    Danbooru: refactor resource initialization.

--- lib/danbooru.rb
+++ lib/danbooru.rb
@@ -15,25 +15,14 @@ class Danbooru
     tags uploads users user_feedbacks wiki_pages wiki_page_versions
   ]
 
-  RESOURCES.each do |name|
-    unless Danbooru::Resource.const_defined?(name.camelize)
-      Danbooru::Resource.const_set(name.camelize, Class.new(Danbooru::Resource))
-    end
-
-    define_method(name) do
-      instance_variable_set("@#{name}", build_resource(name)) unless instance_variable_defined?("@#{name}")
-      instance_variable_get("@#{name}")
-    end
-  end
-
-  attr_reader :http, :url, :user, :api_key, :factory, :log
+  attr_reader :url, :user, :api_key, :log, :http, :resources, :factory
 
   def initialize(url: ENV["BOORU_URL"], user: ENV["BOORU_USER"], api_key: ENV["BOORU_API_KEY"], factory: {}, log: Logger.new(nil))
     url ||= "https://danbooru.donmai.us"
 
     @url, @user, @api_key, @log = Addressable::URI.parse(url), user, api_key, log
     @http = Danbooru::HTTP.new(url, user: user, pass: api_key, log: log)
-    @factory = factory
+    @factory, @resources = factory, {}
   end
 
   def ping(params = {})
@@ -45,9 +34,15 @@ class Danbooru
     users.index(name: user).succeeded?
   end
 
-  private
-  def build_resource(name)
-    resource_class = Danbooru::Resource.const_get(name.camelize)
-    resource_class.new(name, self)
+  def [](name)
+    resources[name.camelize] ||= Resource.const_get(name.camelize).new(name, self)
+  end
+
+  RESOURCES.each do |name|
+    Resource.const_set(name.camelize, Class.new(Resource)) unless Resource.const_defined?(name.camelize)
+
+    define_method(name) do
+      self[name]
+    end
   end
 end
