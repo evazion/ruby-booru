a5881a3e04600791c660af887192af4bf34c4788
evazion <noizave@gmail.com>
2017-12-11 18:37:08 -0600

    Danbooru::Model: fix JSON generation and pretty printing.

--- lib/danbooru/model.rb
+++ lib/danbooru/model.rb
@@ -21,7 +21,8 @@ class Danbooru
     end
 
     def update(params = {}, options = {})
-      self.attributes = api.update(id, { resource_name => params }, options)
+      response = api.update(id, { resource_name => params }, options)
+      self.attributes = response.model.as_json
       self
     end
 
@@ -33,8 +34,15 @@ class Danbooru
       "#{resource_name} ##{id}"
     end
 
-    protected
+    def as_json(options = {})
+      attributes.to_h.transform_values(&method(:serialize_attribute))
+    end
 
+    def pretty_print(printer)
+      printer.pp(attributes.to_h)
+    end
+
+    protected
     def cast_attributes(attributes)
       OpenStruct.new(attributes.map do |name, value|
         [name, cast_attribute(name, value)]
@@ -46,8 +54,23 @@ class Danbooru
         Time.parse(value) rescue nil
       elsif name =~ /(^|_)url$/
         Addressable::URI.parse(value)
+      elsif value.is_a?(Hash)
+        Danbooru::Model.new(value, nil)
       elsif value.is_a?(Array)
-        value.map { |item| Danbooru::Model.new(item, nil) }
+        value.map { |item| cast_attribute(name, item) }
+      else
+        value
+      end
+    end
+
+    def serialize_attribute(value)
+      case value
+      when Time, Addressable::URI
+        value.to_s
+      when Danbooru::Model
+        value.as_json
+      when Array
+        value.map(&method(:serialize_attribute))
       else
         value
       end
