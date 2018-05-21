8f79c765c32dd2c609567168fb042bfd9be95c3e
evazion <noizave@gmail.com>
2018-05-20 20:37:23 -0500

    posts: add search method.

--- lib/danbooru/resource/posts.rb
+++ lib/danbooru/resource/posts.rb
@@ -1,6 +1,10 @@
 require "danbooru/resource"
 
 class Danbooru::Resource::Posts < Danbooru::Resource
+  def search(workers: 2, by: :page, **params)
+    all(workers: workers, by: by, **params)
+  end
+
   def tag(id, tags)
     tags = tags.join(" ") if tags.is_a?(Array)
     update(id, "post[old_tag_string]": "", "post[tag_string]": tags)
