79fa28f73d465cbc69aa6d491333119df171d59f
evazion <noizave@gmail.com>
2018-10-27 14:52:09 -0500

    post: remove absolute_file_url methods.

--- lib/danbooru/model/post.rb
+++ lib/danbooru/model/post.rb
@@ -1,18 +1,6 @@
 require "danbooru/model"
 
 class Danbooru::Model::Post < Danbooru::Model
-  def absolute_preview_file_url
-    booru.url + preview_file_url
-  end
-
-  def absolute_large_file_url
-    booru.url + large_file_url
-  end
-
-  def absolute_file_url
-    booru.url + file_url
-  end
-
   def fav_ids
     fav_string.split.grep(/([0-9]+)/) { $1.to_i }
   end
